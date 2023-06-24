825cdbef39ad43d5ec40ab9df8617abb83b10212
Emerson Gonzalez <emerson.dvlmt@gmail.com>
2023-06-24 12:31:22 -0400

    Reestructura de directorios

--- modelo/curso/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alura.modelo.curso;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alura.modelo.curso.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-
-}
--- modelo/topico/TopicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.modelo.topico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
-
-}
